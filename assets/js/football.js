c4370719a171c6577f13264c7ab73735bc742064
taher2552 <stardom.taher@gmail.com>
2023-12-06 13:03:23 +0530

    changes

--- assets/js/football.js
+++ assets/js/football.js
@@ -74,11 +74,12 @@ document.addEventListener("DOMContentLoaded", function () {
 // view and hide city list js ends
 
 ///blog scroll js
-
 document.addEventListener("DOMContentLoaded", function () {
   const scrollLeftButton = document.querySelector(".scroll-left");
   const scrollRightButton = document.querySelector(".scroll-right");
   const blogFlex = document.querySelector(".fb_blog_flex");
+  const image1 = document.querySelector(".scroll-left img");
+  const image2 = document.querySelector(".scroll-right img");
 
   scrollLeftButton.addEventListener("click", function () {
     blogFlex.scrollBy({
@@ -93,6 +94,26 @@ document.addEventListener("DOMContentLoaded", function () {
       behavior: "smooth",
     });
   });
-});
 
-///blog scroll js ends
+  blogFlex.addEventListener("scroll", function () {
+    const scrollableWidth = blogFlex.scrollWidth - blogFlex.clientWidth;
+    if (blogFlex.scrollLeft === 0) {
+      scrollLeftButton.classList.add("fb_grey_bg");
+      scrollLeftButton.classList.remove("fb_red_bg");
+      image1.src = "assets/images/angel_2.svg";
+      image2.src = "assets/images/angel_1.svg";
+    } else if (blogFlex.scrollLeft >= scrollableWidth) {
+      scrollRightButton.classList.add("fb_grey_bg");
+      scrollRightButton.classList.remove("fb_red_bg");
+      image1.src = "assets/images/angel_2_c.svg";
+      image2.src = "assets/images/angel_1_c.svg";
+    } else {
+      scrollLeftButton.classList.remove("fb_grey_bg");
+      scrollLeftButton.classList.add("fb_red_bg");
+      scrollRightButton.classList.remove("fb_grey_bg");
+      scrollRightButton.classList.add("fb_red_bg");
+      image1.src = "assets/images/angel_2_c.svg";
+      image2.src = "assets/images/angel_1.svg";
+    }
+  });
+});
